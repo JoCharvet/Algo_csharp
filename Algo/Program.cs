b30f87a4437c7fc2cadee64851c43840211de030
Charvet Johnny <Charvet Johnny@CRM-UC-3617>
2022-05-17 08:20:22 +0200

    Ajoutez des fichiers projet.

---
+++ Algo/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+string str_util="";
+string result = " est un palindrome";
+int i=0;
+int n=0;
+
+//input control , if the last char of the string are not a dot or if the string are not long of two char
+while(str_util.Length <2 || str_util.Substring(str_util.Length-1) != ".")
+{
+    Console.WriteLine("Veuillez saisir une phrase terminant par un .");
+    str_util = Console.ReadLine();
+}
+str_util.ToUpper();
+n = str_util.Length - 2;
+
+
+// compare the first char of the string and the last , if they have the same value we continue until the middle of the word, if not its not a palindrome
+while(i!=n || i > n)
+{
+    if (str_util[i]!= str_util[n])
+    {
+        result=" n'est pas un palindrome";
+        n = 2;
+    }
+    i++;
+    n--;
+}
+
+Console.WriteLine(str_util + result);
+Console.ReadKey();
